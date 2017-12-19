d4f720b5306b4decea54bf12963d4e33d1b30f90
Ryan Williams <ryan.williams@projectpeople.com>
2017-12-19 15:55:11 +0000

    Injecting the Authentication object is probably sensible, but my only concern here is that getting the roles is quite an arduous task, there must be an easier way? Failing that, it may be necessary to make a service to handle that.

--- src/main/java/com/spring4inaction/springbootwebmvc/config/SecurityConfig.java
+++ src/main/java/com/spring4inaction/springbootwebmvc/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.spring4inaction.springbootwebmvc.config;
 
         import com.spring4inaction.springbootwebmvc.security.CustomAuthenticationProvider;
+        import org.springframework.context.annotation.Bean;
         import org.springframework.context.annotation.Configuration;
         import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
         import org.springframework.security.config.annotation.web.builders.HttpSecurity;
         import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
         import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         System.out.println("We're going to configure authentication to use our custom authentication provider " + authenticationProvider);
         auth.authenticationProvider(authenticationProvider);
+        //https://medium.com/@gustavo.ponce.ch/spring-boot-spring-mvc-spring-security-mysql-a5d8545d837d hints that this should be done with JPA.
 //        auth.inMemoryAuthentication().withUser("user1").password("test1234")
 //                .roles("USER").and().withUser("admin1").password("test1234")
 //                .roles("ADMIN");
@@ -36,5 +39,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin().loginPage("/login")
                 .and().logout().logoutSuccessUrl("/loggedOut");
     }
+
 
 }
--- src/main/java/com/spring4inaction/springbootwebmvc/controllers/HomeController.java
+++ src/main/java/com/spring4inaction/springbootwebmvc/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.spring4inaction.springbootwebmvc.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,16 +11,18 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class HomeController {
 
-    private HttpServletRequest request;
-
-    public HomeController(HttpServletRequest request){
-        this.request = request;
-    }
-
     @RequestMapping("/")
-    public String home(Model model){
-        boolean isAdmin = request.isUserInRole("ADMIN");
-        String username = request.getUserPrincipal()!=null?request.getUserPrincipal().getName():null;
+    public String home(Model model, Authentication authentication){
+        boolean isAdmin = false;
+        if (authentication!=null && authentication.getAuthorities()!=null){
+            for (GrantedAuthority p : authentication.getAuthorities()){
+                System.out.println("GRANTED AUTHORITY IS " + p.getAuthority());
+                if (p.getAuthority().equals("ROLE_ADMIN")){
+                    isAdmin = true;
+                }
+            }
+        }
+        String username = authentication!=null && authentication.getPrincipal()!=null?authentication.getPrincipal().toString():null;
         model.addAttribute("isAdmin", isAdmin);
         model.addAttribute("username", username);
         return "home";
